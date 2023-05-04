da3b672096ddebb532780f85ed174f83b73a7662
Ajith Revildo <91174414+AjithRevildo@users.noreply.github.com>
2023-05-04 16:28:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { spawn } = require('child_process');
 
 // enable CORS for requests coming from http://localhost:3000
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://ajithrevildo.netlify.app/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
